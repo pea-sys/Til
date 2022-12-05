905819e40822054c4e39a8583d3e52b74e00eee6
pea-sys <49807271+pea-sys@users.noreply.github.com>
2022-12-05 19:33:03 +0900

    Add files via upload

---
+++ wordcompress/doc-to-docx.ps1
@@ -0,0 +1,23 @@
+$targetPath = $Args[0]
+
+$files = Get-ChildItem -Path $targetPath | ? { $_.Extension -like "*.doc" }
+
+$wdFormatDocumentDefault = 16
+ 
+foreach ($f in $files)
+{
+    Write-Host $f
+
+    $word = New-Object -ComObject Word.Application
+
+    $doc = $word.Documents.Open($f.FullName)
+
+    $outputfile = $f.FullName.Replace(".doc", ".docx")
+    Write-Host $outputfile
+
+    $doc.SaveAs2([ref]$outputfile, [ref]$wdFormatDocumentDefault)
+
+    $doc.Close()
+
+    $word.Quit()
+}
