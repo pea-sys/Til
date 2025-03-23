16a7439976a7214426f45c87e983ced7432b62ee
pea-sys <49807271+pea-sys@users.noreply.github.com>
2025-03-24 07:28:52 +0900

    Add files via upload

---
+++ 流麗なjavascript/5-1.js
@@ -0,0 +1,4 @@
+function flatten(arr) {
+  return arr.reduce((acc, val) => acc.concat(Array.isArray(val) ? flatten(val) : val), []);
+}
+console.log(flatten([1, [2, [3, 4], [[5]]]])); // [1, 2, 3, 4, 5]
