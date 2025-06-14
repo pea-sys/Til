ea245d9f94ed9f074e258f04208f290406682a26
pea-sys <49807271+pea-sys@users.noreply.github.com>
2025-06-14 17:57:06 +0900

    Add files via upload

---
+++ ConcurrentGo/ch12/2/2.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"runtime"
+	"sync"
+	"sync/atomic"
+)
+
+type SpinLock atomic.Bool
+
+func (s *SpinLock) Lock() {
+	for !(*atomic.Bool)(s).CompareAndSwap(false, true) {
+		runtime.Gosched()
+	}
+}
+
+func (s *SpinLock) Unlock() {
+	(*atomic.Bool)(s).Store(false)
+}
+
+func (s *SpinLock) TryLock() bool {
+	return (*atomic.Bool)(s).CompareAndSwap(false, true)
+}
+
+func NewSpinLock() sync.Locker {
+	var lock SpinLock
+	return &lock
+}
